82aee232ba6de69907998d77c806b0f27ceab593
Shreyas Mavle <shreyasmavle007@gmail.com>
2024-06-15 19:46:26 +0530

    added stubs

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+app.post('/api/questions', (req, res) => {
+	res.send('Accept user question, and return AI-generated answer');
+});
+
+app.get('/api/questions/:questionId', (req, res) => {
+	res.send('Retrieve specific question and answer by question ID');
+});
+
+app.post('/api/users', (req, res) => {
+	res.send('Create a new user account');
+});
+
+app.get('/api/users/:userId', (req, res) => {
+	res.send('Retrieve a user profile with a given userId');
+});
+
+app.get('/api/users/:userId/questions', (req, res) => {
+	res.send('Retrieve all questions asked by user with a given userId');
 });
 
 app.listen(port, () => {
